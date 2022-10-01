1a06659d9fcebf64a8bbfd1a37d02ed81a11dadf
Sunny kumar <98548654+Sunnykumar17@users.noreply.github.com>
2022-10-01 15:17:56 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <div className="card footer">
+        <h2>GitFind | Developed by Sunny &amp; Team | &copy; 2022</h2>
+        <a href="https://github.com/Sunnykumar17/gitfind.git" target="_blank" rel="noreferrer">Github Source Code</a>
+        <p> Made with ❤️ &amp; 💻</p>
+      </div>
+    )
+  }
+  
+  export default Footer;
